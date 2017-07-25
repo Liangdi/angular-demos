370dc7746c1f0b3f18835dbae120778da94d4356
Liangdi <wu@liangdi.me>
2017-07-26 00:39:01 +0800

    update i18n-demo

--- i18n-demo/src/app/app.component.ts
+++ i18n-demo/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { OnInit } from "@angular/core/src/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  constructor(private translate: TranslateService) {
+
+  }
+
+  ngOnInit(): void {
+   let lan =   this.translate.getBrowserLang();
+   console.log("lan:",lan);
+
+   this.translate.addLangs(["en", "zh_CN"]);
+   this.translate.setDefaultLang('zh_CN');
+  }
 }
